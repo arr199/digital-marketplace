ce566e4ea3109019e01cb36711a116b0e0f2e484
arr199 <abieledelso@gmail.com>
2024-10-03 08:17:52 +0100

    add memory optimizations

--- next.config.js
+++ next.config.js
@@ -1,28 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images : {
-        remotePatterns : [
-            {
-                hostname : "digital-marketplace.up.railway.app",
-                pathname : "/**" ,
-                port : "" ,
-                protocol : "https",
-            },
-            {
-                hostname : "localhost",
-                pathname : "**" ,
-                port : "3400" ,
-                protocol : "http",
-            },
-            { 
-                hostname : "digital-marketplace-ghencuyfzq-nw.a.run.app",
-                pathname : "**" ,
-                port : "" ,
-                protocol : "https",
-            },
-        ]
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: "memory",
+      });
     }
-}
+    // Important: return the modified config
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "digital-marketplace.up.railway.app",
+        pathname: "/**",
+        port: "",
+        protocol: "https",
+      },
+      {
+        hostname: "localhost",
+        pathname: "**",
+        port: "3400",
+        protocol: "http",
+      },
+      {
+        hostname: "digital-marketplace-ghencuyfzq-nw.a.run.app",
+        pathname: "**",
+        port: "",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
